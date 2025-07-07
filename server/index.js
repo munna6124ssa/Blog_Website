057a54280f8fbb79911acb013541b7110643313e
munna6124ssa <munna6124ssa@gmail.com>
2025-07-07 15:42:43 +0530

    fix: CORS frontend URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173', 
         'http://localhost:3000',
-        'https://blog-website-frontend.onrender.com'
+        'https://blog-website-frontend-s1r8.onrender.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
